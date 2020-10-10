588725e5a0876bf8defd8c2540d01e40fdc1207d
Long Nguyen <lpn@mit.edu>
2020-10-10 02:39:55 -0400

    Finished Lab 1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,3 +1,5 @@
+import java.util.stream.IntStream;
+
 /** Class that determines whether or not a year is a leap year.
  *  @author YOUR NAME HERE
  */
@@ -14,20 +16,30 @@ public class LeapYear {
         }
     }
 
+    /**
+     * Calculates whether or not a year is a leap year.
+     *
+     * @param year Year to calculate leap year validity of.
+     * @return Whether or not year is a leap year.
+     */
+    public static boolean isLeapYear(int year) {
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("Please enter command line arguments.");
             System.out.println("e.g. java Year 2000");
         }
-        for (int i = 0; i < args.length; i++) {
+        IntStream.range(0, args.length).forEach(i -> {
             try {
                 int year = Integer.parseInt(args[i]);
                 checkLeapYear(year);
             } catch (NumberFormatException e) {
                 System.out.printf("%s is not a valid number.\n", args[i]);
             }
-        }
+        });
     }
 }
 

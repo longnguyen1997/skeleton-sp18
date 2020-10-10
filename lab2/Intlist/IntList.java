205fe985306c3b84a05a3580b106cb0f7862a0da
Long Nguyen <lpn@mit.edu>
2020-10-10 03:30:23 -0400

    dcatenate bug; handled null inputs

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,6 +81,9 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if (A == null) {
+            return B;
+        }
         if (A.rest == null) {
             A.rest = B;
             return A;
